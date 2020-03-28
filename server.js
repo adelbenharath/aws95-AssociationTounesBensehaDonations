5ea3e2d9fabb83be44ff7614ed4b5f56c6f71c4f
ADEL BENHARETH <adel.benhareth@oyez.fr>
2020-03-28 17:52:11 +0100

    change path

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('./dist/tba'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/tba/src/index.html'));
+res.sendFile(path.join(__dirname,'/dist/tba/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
