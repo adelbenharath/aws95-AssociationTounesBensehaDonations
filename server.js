2dc54e715e98162a7a7208b696319ad00f0b1eaf
ADEL BENHARETH <adel.benhareth@oyez.fr>
2020-03-28 17:47:37 +0100

    tba

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/ahna-likom'));
+app.use(express.static('./dist/tba'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/ahna-likom/src/index.html'));
+res.sendFile(path.join(__dirname,'/dist/tba/src/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
